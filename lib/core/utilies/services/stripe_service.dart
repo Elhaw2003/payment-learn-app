e72239b0fdb28872251824f7d34e822b2df67de3
Ibrahim Elhaw <ebrahimelhaw0@gmail.com>
2025-09-03 14:20:37 +0300

    create payment intent done

--- lib/core/utilies/services/stripe_service.dart
+++ lib/core/utilies/services/stripe_service.dart
@@ -1,10 +1,15 @@
+import 'package:payment_learn_app/core/utilies/services/api_keys.dart';
+import 'package:payment_learn_app/core/utilies/services/api_service.dart';
 
-import 'package:payment_learn_app/features/my_cart/data/models/payment_intent_input_model.dart';
-import 'package:payment_learn_app/features/my_cart/data/models/payment_intent_model.dart';
+import '../../../features/my_cart/data/models/payment_intent_input_model.dart';
+import '../../../features/my_cart/data/models/payment_intent_model.dart';
 
 class StripeService {
-  // create payment intent (amount , currency)
-  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel paymentIntentInputModel) {
-  //
-  // }
+  final ApiService apiService = ApiService();
+  // createPaymentIntent (amount , currency)
+  Future<PaymentIntentModel> createPaymentIntent(PaymentIntentInputModel paymentIntentInputModel) async{
+   var response = await apiService.post(body: paymentIntentInputModel.toJson(), url: "https://docs.stripe.com/api/payment_intents/create", token: ApiKeys.secretKey);
+   var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+   return paymentIntentModel;
+  }
 }
